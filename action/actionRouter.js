57ddf8ba394ba4c83d0f4ce8fdd9c314feae80ba
Andrew Maddocks <andrewmadd@icloud.com>
2020-01-17 11:16:05 -0700

    everything is done

--- action/actionRouter.js
+++ action/actionRouter.js
@@ -4,33 +4,83 @@ const Project = require("../data/helpers/projectModel.js");
 const Action = require("../data/helpers/actionModel.js");
 
 const router = express.Router();
-// GET test
+// GET test✅
 router.get("/test", (req, res) => {
   res.send(`
       
         <p>Welcome to the actionRouter API</p>
       `);
 });
-router.get("/", (req, res) => {
-  // do your magic!
+// Get retrieving a list of actions for a project ✅
+router.get("/:id", validateactionId, (req, res) => {
+  const id = req.action.id;
+  Project.getProjectActions(id)
+    .then(action => {
+      if (action[0]) {
+        res.status(200).json(action);
+      } else if (!action[0]) {
+        res.status(404).json({ message: "No project with this ID exists" });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        error: "the action info could not be retrieved"
+      });
+    });
 });
-
-router.get("/:id", (req, res) => {
-  // do your magic!
-});
-
+// Delete action ✅
 router.delete("/:id", (req, res) => {
-  // do your magic!
+  Action.remove(req.params.id)
+    .then(() => {
+      res
+        .status(200)
+        .json({ message: `action with id ${req.params.id} was removed` });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        errorMessage: "the action could not be removed"
+      });
+    });
 });
-
+// Put updates actions ✅
 router.put("/:id", (req, res) => {
-  // do your magic!
+  const id = req.params.id;
+  const action = req.body;
+
+  Action.update(id, action)
+    .then(action => {
+      if (!action) {
+        res.status(404).json({
+          message: "The action with the specified ID does not exist."
+        });
+      } else {
+        res.status(200).json({
+          message: "The action information was updated successfully"
+        });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      //handle the error
+      res.status(500).json({
+        errorMessage: "The action information could not be modified."
+      });
+    });
 });
 
 // custom middleware
 
-function validatePostId(req, res, next) {
-  // do your magic!
+//make sure the Id exists middleware ✅
+function validateactionId(req, res, next) {
+  Action.get(req.params.id)
+    .then(pj => {
+      req.action = pj;
+      next();
+    })
+    .catch(error => {
+      res.status(500).json({ message: "need to give an ID" });
+    });
 }
-
 module.exports = router;
